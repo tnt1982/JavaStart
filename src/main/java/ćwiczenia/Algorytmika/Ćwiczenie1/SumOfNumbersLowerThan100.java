17b1e72053fa716e9bcd8c85ba3964c181cf8223
tnt1982 <97390681+tnt1982@users.noreply.github.com>
2022-10-20 19:27:22 +0200

    lekcje 20-10-2022;

---
+++ src/main/java/ćwiczenia/Algorytmika/Ćwiczenie1/SumOfNumbersLowerThan100.java
@@ -0,0 +1,25 @@
+package ćwiczenia.Algorytmika.Ćwiczenie1;
+
+import java.util.Scanner;
+
+class CheckSum {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int next = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę ");
+
+        while((next = scanner.nextInt()) <= 100) {
+            System.out.println("Podaj liczbę: ");
+            sum += next;
+        }
+
+        int mod = sum % 2;
+        if(mod == 0) {
+            System.out.println(sum + " parzysta");
+        } else {
+            System.out.println(sum + " nieparzysta");
+        }
+    }
+}
---
+++ src/main/java/ćwiczenia/Algorytmika/Ćwiczenie1/SumOfNumbersLowerThanHundred.java
@@ -0,0 +1,33 @@
+package ćwiczenia.Algorytmika.Ćwiczenie1;
+
+import java.util.Scanner;
+
+public class SumOfNumbersLowerThanHundred {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+
+        while (true) {
+            System.out.println("\nPodaj liczbę całkowitą");
+            int number = scanner.nextInt();
+            sum += number;
+
+            if (sum >= 100) {
+                System.out.println("Suma cyfr wynosi " + sum + ", jest równa, lub większa niż 100!!!. " +
+                        "Muszę zakończyć program.");
+                return;
+            }
+            if (sum % 2 == 0) {
+                System.out.println("Suma liczb jest parzysta");
+            } else {
+                System.out.println("Suma liczb jest nieparzysta");
+            }
+            System.out.println("Suma liczb wynosi " + sum);
+
+        }
+    }
+}
+
