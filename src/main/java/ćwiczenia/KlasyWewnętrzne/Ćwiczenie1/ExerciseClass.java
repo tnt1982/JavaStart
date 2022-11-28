70a52078e7c12b4782f08ab71879e04a39a30da9
tnt1982 <97390681+tnt1982@users.noreply.github.com>
2022-11-28 20:10:50 +0100

    lekcje 28-11-2022;

--- src/main/java/ćwiczenia/KlasyWewnętrzne/Ćwiczenie1/ExerciseClass.java
+++ src/main/java/ćwiczenia/KlasyWewnętrzne/Ćwiczenie1/ExerciseClass.java
@@ -1,10 +1,23 @@
 package ćwiczenia.KlasyWewnętrzne.Ćwiczenie1;
 
+import java.util.Scanner;
+
 public class ExerciseClass {
 
     public static void main(String[] args) {
 
-        int[] tab = {7777, 4, 7, 1, 55, -99, 925, 789};
+        int tabCapacity = 7;
+        int[] tab = new int[tabCapacity];
+        int numberOfNumber = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (numberOfNumber < tabCapacity) {
+            System.out.println("Podaj liczbę, którą dodamy do tablicy");
+            int number = scanner.nextInt();
+            tab[numberOfNumber] = number;
+            numberOfNumber++;
+        }
 
         Sortable s = new Sortable() { // klasa anonimowa;
             @Override
@@ -24,12 +37,13 @@ public class ExerciseClass {
             }
         };
         printArray(tab);
-        System.out.println("Najwyższą wartością jest:" );
+        System.out.println("\nNajwyższą wartością z tablicy jest:");
         s.sort(tab);
 
     }
 
     private static void printArray(int[] tab) {
+        System.out.println("\nWszystkie wpisane liczby z tablicy: ");
         for (int value : tab) {
             System.out.println(value);
         }
