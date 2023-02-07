92530081a331b020375070b89dd84894611eb365
tnt1982 <97390681+tnt1982@users.noreply.github.com>
2023-02-07 21:06:51 +0100

    lekcje 07.02.2023

---
+++ src/main/java/ćwiczenia/typyGeneryczne/ćwiczenie2/Test.java
@@ -0,0 +1,31 @@
+package ćwiczenia.typyGeneryczne.ćwiczenie2;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Pair<String, Integer> firstPair = new Pair<>("Pimpuś", 3);
+        firstPair.printPair();
+
+        Pair<String, Integer> secondPair = new Pair<>("Azorek", 4);
+        secondPair.printPair();
+
+        Pair<Double, Double> thirdPair = new Pair<>(7.5, 7.1);
+        thirdPair.printPair();
+
+        double v = thirdPair.getV();
+        double t = thirdPair.getV();
+        System.out.println(v + t);
+
+        System.out.println("Drukowanie z metody generycznej: ");
+        printPairs(firstPair);
+        printPairs(secondPair);
+        printPairs(thirdPair);
+
+    }
+
+    private static <T, V> void printPairs(Pair<T, V> pairToPrint) {
+        System.out.println(pairToPrint.getT() + " " + pairToPrint.getV());
+    }
+
+}
