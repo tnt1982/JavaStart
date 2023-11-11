63390968b0d27eaff14a26afa6ec10c575493b70
Toshiba Satellite <tnt1982@o2.pl>
2023-11-11 17:44:20 +0100

    poprawka 11-11-2023

--- src/main/java/ćwiczenia/Algorytmika/Ćwiczenie0/SumaLiczbParzystych.java
+++ src/main/java/ćwiczenia/Algorytmika/Ćwiczenie0/SumaLiczbParzystych.java
@@ -1,5 +1,7 @@
 package ćwiczenia.Algorytmika.Ćwiczenie0;
 
+import java.util.Arrays;
+
 public class SumaLiczbParzystych {
 
     public static void main(String[] args) {
@@ -14,20 +16,26 @@ public class SumaLiczbParzystych {
         }
         System.out.print("Suma liczb parzystych z pętli for wynosi: " + sum);
 
-        System.out.println(" ");
+        System.out.println("\n");
 
         System.out.println("Iterujemy wszystkie liczby z tablicy:");
         for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
+            System.out.print(numbers[i] + ", ");
         }
 
-        System.out.println(" ");
+        System.out.println("\n");
 
+        System.out.println("drukujemy wszystkie liczby z tablicy numbers przy użyciu streamów");
+        Arrays.stream(numbers).forEach(System.out::print);
+
+        System.out.println("\n");
+
+        int sumFromLoop = 0;
         for (int element : numbers) {
             if (element % 2 == 0) {
-                sum = sum + element;
+                sumFromLoop += element;
             }
         }
-        System.out.println("Suma liczb parzystych z pętli for-each wynosi: " + (sum - 50));
+        System.out.println("Suma liczb parzystych z pętli for-each wynosi: " + (sumFromLoop));
     }
 }
